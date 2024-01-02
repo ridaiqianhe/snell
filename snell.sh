3d2cf6f586b7e8d4a12d8bfc21c0f9234e33a4c4
ridaiqianhe <121543611+ridaiqianhe@users.noreply.github.com>
2024-01-03 03:38:44 +0800

    Update snell.sh

--- snell.sh
+++ snell.sh
@@ -240,10 +240,10 @@ services:
     environment:
       - MODE=server
       - V3=1
-      - LISTEN=0.0.0.0:$PORT_NUMBER   
+      - LISTEN=0.0.0.0:8443  
       - SERVER=127.0.0.1:$PORT_NUMBER
       - TLS=mp.weixin.qq.com:443
-      - PASSWORD=$PASSWORD 
+      - PASSWORD=weixin
 EOF
 
   mkdir -p ./snell-conf || { echo "Error: Unable to create directory $NODE_DIR/snell-conf"; exit 1; }
